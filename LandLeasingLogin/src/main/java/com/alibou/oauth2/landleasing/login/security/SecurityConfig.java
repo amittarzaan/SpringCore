761ea1e55622232849897641cbddfe684da31290
05923R744 <05923R744@host.docker.internal>
2023-10-28 13:10:01 +0530

    commited code

--- LandLeasingLogin/src/main/java/com/alibou/oauth2/landleasing/login/security/SecurityConfig.java
+++ LandLeasingLogin/src/main/java/com/alibou/oauth2/landleasing/login/security/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.alibou.oauth2.landleasing.login.security;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +27,42 @@ public class SecurityConfig {
         .authenticated()
         .and()
         .oauth2Login()
-    ;
+	    .successHandler(new AuthenticationSuccessHandler()
+	    		{
+			
+			public void onAuthenticationSuccess(jakarta.servlet.http.HttpServletRequest request,
+					jakarta.servlet.http.HttpServletResponse response, Authentication authentication)
+					throws IOException, jakarta.servlet.ServletException {
+				System.out.println("AuthenticationSuccessHandler invoked");
+				System.out.println("Authentication name: " + authentication.getName());
+				//Principal p=(Principal) authentication.getPrincipal();
+				 OAuth2AuthenticatedPrincipal principal=(OAuth2AuthenticatedPrincipal) authentication.getPrincipal();
+				try {
+					//CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
+					System.out.println("principal>>>"+principal.getAttribute("name"));
+					System.out.println("principal>>>"+principal.getAttribute("family_name"));
+					System.out.println("principal>>>"+principal.getAttribute("email"));
+					System.out.println("principal>>>"+principal.getAttribute("picture"));
+					System.out.println("principal>>>"+principal.getAttribute("email_verified"));
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+								
+				//userService.processOAuthPostLogin(principal);
+				response.sendRedirect("/dynamic/api/profile");
+			}
+	    	
+	    		})
+	    .and()
+		.logout().logoutSuccessUrl("/").permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/403")
+		;
+        
+    
 
     System.out.println("calling SecurityConfig...AMIT LAHA1");
     return http.build();
-  }
+    }
 }
